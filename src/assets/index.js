d0898bc46eb35fce5aa36543150d842748efd167
ozerbt <54027925+ozerbt@users.noreply.github.com>
2023-03-03 20:43:24 -0500

    integrated three.js

---
+++ src/assets/index.js
@@ -0,0 +1,67 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import peel9 from "./company/peel9.png";
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import peel9img from "./peel9.png"
+import osdp from "./osdp.png"
+import yumar from "./yumar.png"
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import nathan from "./nathan_wick.png"
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import vue from "./tech/vue.png"
+import pni from "./company/pni.png";
+import python from "./tech/python.png"
+import sql from "./tech/sql.png"
+import itsc from "./company/itsc.png"
+import angular from "./tech/angular.png"
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  python,
+  vue,
+  sql,
+  angular,
+  itsc,
+  web,
+  github,
+  peel9,
+  nathan,
+  peel9img,
+  osdp,
+  yumar,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  pni,
+  typescript,
+  threejs,
+  tripguide,
+};
